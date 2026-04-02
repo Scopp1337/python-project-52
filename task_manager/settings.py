49c2b400a0a9834282ec06e3c2cf2543c15fb5ad
Roman <roma.mazin.47@gmail.com>
2026-04-02 16:37:03 +0300

    Fix render error

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
