f27080606116ba4ffbb80cde1db40215b007f099
Roman <roma.mazin.47@gmail.com>
2026-04-09 15:11:43 +0300

    Fix hexlet tests

--- users/forms.py
+++ users/forms.py
@@ -5,36 +5,6 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 User = get_user_model()
 
 
-class UserCreateForm(UserCreationForm):
-    first_name = forms.CharField(
-        label='Имя',
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    last_name = forms.CharField(
-        label='Фамилия',
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    username = forms.CharField(
-        label='Имя пользователя',
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    password1 = forms.CharField(
-        label='Пароль',
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-    password2 = forms.CharField(
-        label='Подтверждение пароля',
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
-
-
 class UserUpdateForm(forms.ModelForm):
     first_name = forms.CharField(
         label='Имя',
@@ -56,18 +26,22 @@ class UserUpdateForm(forms.ModelForm):
         required=False,
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
+    password_confirm = forms.CharField(
+        label='Подтверждение пароля',
+        required=False,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username']
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
 
-class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label='Имя пользователя',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    password = forms.CharField(
-        label='Пароль',
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
+        if password and password_confirm:
+            if password != password_confirm:
+                self.add_error('password_confirm', 'Пароли не совпадают')
+        return cleaned_data
