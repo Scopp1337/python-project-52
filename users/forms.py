8c40ed7010794a9eac0e5141c7d2bb1765900e1d
Roman <roma.mazin.47@gmail.com>
2026-04-09 14:13:19 +0300

    Fix hexlet tests

--- users/forms.py
+++ users/forms.py
@@ -1,118 +1,26 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
 
 class UserCreateForm(UserCreationForm):
-    first_name = forms.CharField(
-        max_length=150,
-        required=True,
-        label='Имя',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    last_name = forms.CharField(
-        max_length=150,
-        required=True,
-        label='Фамилия',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    username = forms.CharField(
-        max_length=150,
-        required=True,
-        label='Имя пользователя',
-        help_text=(
-            'Обязательное поле. Не более 150 символов. '
-            'Только буквы, цифры и символы @/./+/-/_.'
-        ),
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    password1 = forms.CharField(
-        label='Пароль',
-        help_text='Ваш пароль должен содержать как минимум 3 символа.',
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-    password2 = forms.CharField(
-        label='Подтверждение пароля',
-        help_text='Для подтверждения введите, пожалуйста, пароль ещё раз.',
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username',
-                  'password1', 'password2']
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        if User.objects.filter(username=username).exists():
-            raise ValidationError(
-                'Пользователь с таким именем уже существует'
-            )
-        return username
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password1 = cleaned_data.get('password1')
-        password2 = cleaned_data.get('password2')
-
-        if password1 and password2:
-            if len(password1) < 3:
-                self.add_error(
-                    'password1',
-                    'Введенный пароль слишком короткий'
-                )
-            if password1 != password2:
-                self.add_error(
-                    'password2',
-                    'Введенные пароли не совпадают'
-                )
-        return cleaned_data
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
-    first_name = forms.CharField(
-        max_length=150,
-        required=True,
-        label='Имя',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    last_name = forms.CharField(
-        max_length=150,
-        required=True,
-        label='Фамилия',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    username = forms.CharField(
-        max_length=150,
-        required=True,
-        label='Имя пользователя',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username']
 
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        if User.objects.filter(username=username).exclude(
-            pk=self.instance.pk
-        ).exists():
-            raise ValidationError(
-                'Пользователь с таким именем уже существует'
-            )
-        return username
-
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(
-        label='Имя пользователя',
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     password = forms.CharField(
-        label='Пароль',
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
