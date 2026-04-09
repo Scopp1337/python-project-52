b8cba93f0415887507971dd979d1f95aee0da839
Roman <roma.mazin.47@gmail.com>
2026-04-09 13:30:16 +0300

    Add new workflow

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,9 +33,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'webserver',
+]
 
 
 # Application definition
